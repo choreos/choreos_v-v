ddff98a32dea3eb4029b8d6ca22912ab8b267452
Felipe Besson <flpbesson@gmail.com>
2011-08-06 13:00:19 -0300

    package names changed to the prefix eu.choreos

--- choreosvv/src/main/java/br/usp/ime/choreos/vv/exceptions/InvalidOperationNameException.java
+++
@@ -1,7 +0,0 @@
-package br.usp.ime.choreos.vv.exceptions;
-
-public class InvalidOperationNameException extends Exception {
-
-	private static final long serialVersionUID = -3562272248519682510L;
-
-}
