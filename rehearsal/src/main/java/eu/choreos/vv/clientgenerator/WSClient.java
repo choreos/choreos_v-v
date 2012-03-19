23a869f86a7f86a6d491c805a7567e553b39245e
Felipe Besson <besson@batcave.(none)>
2012-03-19 14:11:27 -0300

    pom.xml adjustments

--- rehearsal/src/main/java/eu/choreos/vv/clientgenerator/WSClient.java
+++ rehearsal/src/main/java/eu/choreos/vv/clientgenerator/WSClient.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JOptionPane;
-
 import org.apache.xmlbeans.XmlException;
 
 import com.eviware.soapui.impl.WsdlInterfaceFactory;
