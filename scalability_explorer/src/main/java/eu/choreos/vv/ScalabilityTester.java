d750f5da91a91cb95c6b78688c5ba5caad4acc6f
Paulo Moura <pbmoura@gmail.com>
2012-08-22 17:51:56 -0300

    statistical function, scaling function and load generator can be chosen

--- scalability_explorer/src/main/java/eu/choreos/vv/ScalabilityTester.java
+++ scalability_explorer/src/main/java/eu/choreos/vv/ScalabilityTester.java
@@ -4,76 +4,113 @@ import java.util.ArrayList;
 import java.util.List;
 
 import eu.choreos.vv.aggregations.Aggregator;
+import eu.choreos.vv.aggregations.Mean;
 import eu.choreos.vv.aggregations.Percentile;
 import eu.choreos.vv.chart.ScalabilityReportChart;
 import eu.choreos.vv.increasefunctions.LinearIncrease;
+import eu.choreos.vv.increasefunctions.ScalabilityFunction;
 import eu.choreos.vv.loadgenerator.LoadGenerator;
 import eu.choreos.vv.loadgenerator.UniformLoadGenerator;
 import eu.choreos.vv.loadgenerator.executable.LatencyMeasurementExecutable;
 
 public abstract class ScalabilityTester implements ScalabilityTestItem {
-	
-	private int numberOfExecutions;
-	
-	private Exception lastException;
-	
-	private List<ScalabilityReport> reports;
-	
-	public void setUp() throws Exception {}
 
-	public void resourceScaling(int resourceQuantity) throws Exception {}
-	
-	public void beforeTest() throws Exception {}
-	
-	public void test() throws Exception {}
-	
-	public void tearDown() throws Exception {}
-	
+	private int numberOfExecutions;
+	private LoadGenerator loadGen;
+	private Aggregator aggregator;
+	private ScalabilityFunction function;
+
+	private Exception lastException;
+
+	private List<ScalabilityReport> reports;
+
+	public void setUp() throws Exception {
+	}
+
+	public void resourceScaling(int resourceQuantity) throws Exception {
+	}
+
+	public void beforeTest() throws Exception {
+	}
+
+	public void test() throws Exception {
+	}
+
+	public void tearDown() throws Exception {
+	}
+
 	public ScalabilityTester() {
-		reports = new ArrayList<ScalabilityReport>();
+		this(new UniformLoadGenerator(), new Mean(), new LinearIncrease());
 	}
 	
-	
+	public ScalabilityTester(LoadGenerator loadGenerator, Aggregator aggregator, ScalabilityFunction function) {
+		this.loadGen = loadGenerator;
+		this.aggregator = aggregator;
+		this.function = function;
+		reports = new ArrayList<ScalabilityReport>();
+	}
+
+	public LoadGenerator getLoadGen() {
+		return loadGen;
+	}
+
+	public void setLoadGen(LoadGenerator loadGen) {
+		this.loadGen = loadGen;
+	}
+
+	public Aggregator getAggregator() {
+		return aggregator;
+	}
+
+	public void setAggregator(Aggregator aggregator) {
+		this.aggregator = aggregator;
+	}
+
+	public ScalabilityFunction getFunction() {
+		return function;
+	}
+
+	public void setFunction(ScalabilityFunction function) {
+		this.function = function;
+	}
+
 	@Override
 	public double test(Number... params) throws Exception {
 		int requestsPerMinute = params[0].intValue();
 		int resourceQuantity = params[1].intValue();
-		LoadGenerator loadGen = new UniformLoadGenerator();
 		List<Number> results = new ArrayList<Number>();
 
 		resourceScaling(resourceQuantity);
-		
-		try {
-			results = loadGen.execute(numberOfExecutions, requestsPerMinute, new LatencyMeasurementExecutable() {
-				@Override
-				public void setUp() throws Exception {
-					beforeTest();
-				}
-				
-				@Override
-				public void experiment() throws Exception {
-					test();
-				}
-			});
-			
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		Aggregator percentile = new Percentile(95);
-		return percentile.aggregate(results);
+
+		results = loadGen.execute(numberOfExecutions, requestsPerMinute,
+				new LatencyMeasurementExecutable() {
+					@Override
+					public void setUp() throws Exception {
+						beforeTest();
+					}
+
+					@Override
+					public void experiment() throws Exception {
+						test();
+					}
+				});
+
+		return aggregator.aggregate(results);
 	}
-	
-	public boolean run(String name, int timesToRun, double latencyLimit, int numberOfExecutionsPerTest, int initialRequestsPerMinute, int inititalResoucesQuantity) {
+
+	public boolean run(String name, int timesToRun, double latencyLimit,
+			int numberOfExecutionsPerTest, int initialRequestsPerMinute,
+			int inititalResoucesQuantity) {
 		numberOfExecutions = numberOfExecutionsPerTest;
-		ScalabilityTest scalabilityTtest = new ScalabilityTest(this, name, timesToRun, latencyLimit, new LinearIncrease());
-		scalabilityTtest.setInitialParametersValues(initialRequestsPerMinute, inititalResoucesQuantity);
-		
+		ScalabilityTest scalabilityTest = new ScalabilityTest(this, name,
+				timesToRun, latencyLimit, function);
+		scalabilityTest.setInitialParametersValues(initialRequestsPerMinute,
+				inititalResoucesQuantity);
+
 		ScalabilityReport report;
 		try {
 			setUp();
-			report = scalabilityTtest.executeIncreasingParams();
+			report = scalabilityTest.executeIncreasingParams();
 			tearDown();
 			reports.add(report);
 		} catch (Exception e) {
@@ -82,12 +119,20 @@ public abstract class ScalabilityTester implements ScalabilityTestItem {
 		}
 		return true;
 	}
-	
+
+	public boolean run(String name, int timesToRun,
+			int numberOfExecutionsPerTest, int initialRequestsPerMinute,
+			int inititalResoucesQuantity) {
+		return run(name, timesToRun, Double.MAX_VALUE,
+				numberOfExecutionsPerTest, initialRequestsPerMinute,
+				inititalResoucesQuantity);
+	}
+
 	public void showChart() {
 		ScalabilityReportChart chart = new ScalabilityReportChart();
-		chart.createChart(reports);		
+		chart.createChart(reports);
 	}
-	
+
 	public Exception getLastException() {
 		return lastException;
 	}
