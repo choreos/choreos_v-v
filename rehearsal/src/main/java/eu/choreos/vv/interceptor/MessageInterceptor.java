3e26d0c50d561f8836d8979b3d589e1b11f787a9
Felipe Besson <flpbesson@gmail.com>
2012-09-22 23:50:30 -0300

    Improving MessageInterceptor feature

--- rehearsal/src/main/java/eu/choreos/vv/interceptor/MessageInterceptor.java
+++ rehearsal/src/main/java/eu/choreos/vv/interceptor/MessageInterceptor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.xmlbeans.XmlException;
 
 import eu.choreos.vv.clientgenerator.Item;
@@ -17,40 +18,43 @@ import eu.choreos.vv.exceptions.WSDLException;
  * This class provides the Message Interceptor feature
  * 
  * @author Felipe Besson
- *
+ * 
  */
 public class MessageInterceptor {
 
 	private WSProxy proxy;
-	private InterceptedMessagesRegistry registry; 
+	private InterceptedMessagesRegistry registry;
 	private String port;
-	
+	private String name;
+
 	/**
 	 * Creates a message interceptor instance which will intercept the messages
 	 * by using a proxy deployed on the provided port
 	 * 
 	 * @param port
 	 */
-	public MessageInterceptor(String port){
+	public MessageInterceptor(String port) {
 		registry = InterceptedMessagesRegistry.getInstance();
 		this.port = port;
 	}
-	
+
 	/**
 	 * Intercepts all messages sent to the provided WSDL
 	 * 
 	 * @param realWsdl
-	 * @throws IOException 
-	 * @throws XmlException 
-	 * @throws WSDLException 
-	 * @throws MockDeploymentException 
+	 * @throws IOException
+	 * @throws XmlException
+	 * @throws WSDLException
+	 * @throws MockDeploymentException
 	 * @throws Exception
 	 */
-	public void interceptTo(String realWsdl) throws WSDLException, XmlException, IOException, MockDeploymentException  {
-		proxy = new WSProxy(WsdlUtils.getBaseName(realWsdl), realWsdl);
+	public void interceptTo(String realWsdl) throws WSDLException,
+			XmlException, IOException, MockDeploymentException {
+
+		proxy = new WSProxy(getProxyName(realWsdl), realWsdl);
 		proxy.setPort(port);
 		proxy.start();
-		
+
 		registry.registerWsdl(realWsdl);
 	}
 
@@ -61,14 +65,16 @@ public class MessageInterceptor {
 	 */
 	public List<Item> getMessages() {
 		List<Item> itemMessages = new ArrayList<Item>();
-		List<String> xmlMessages =  registry.getMessages(proxy.getRealWsdl());
+		List<String> xmlMessages = registry.getMessages(proxy.getRealWsdl());
 		ItemParser parser = new ItemParser();
 
 		try {
 			for (String xmlMessage : xmlMessages)
-					itemMessages.add(parser.parse(xmlMessage));
-		} catch (ParserException e) {e.printStackTrace();}
-		
+				itemMessages.add(parser.parse(xmlMessage));
+		} catch (ParserException e) {
+			e.printStackTrace();
+		}
+
 		return itemMessages;
 	}
 
@@ -83,9 +89,19 @@ public class MessageInterceptor {
 	public String getPort() {
 		return proxy.getPort();
 	}
-	
-	public void stop(){
+
+	public void stop() {
 		proxy.stop();
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String getProxyName(String realWsdl) {
+		if (!StringUtils.isEmpty(name)) {
+			return name;
+		}
+		return WsdlUtils.getBaseName(realWsdl);
+	}
 }
