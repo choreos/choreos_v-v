b9c414a3cb06ccf6c29cffb1fa8b7c803a4b051e
Paulo Moura <pbmoura@gmail.com>
2013-02-03 17:03:06 -0200

    included ANOVA test

---
+++ scalability_explorer/src/main/java/eu/choreos/vv/analysis/ANOVATest.java
@@ -0,0 +1,35 @@
+package eu.choreos.vv.analysis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.math.stat.inference.TestUtils;
+
+import eu.choreos.vv.data.ReportData;
+import eu.choreos.vv.data.ScalabilityReport;
+
+public class ANOVATest implements Analyser {
+
+	@Override
+	public void analyse(List<ScalabilityReport> reports) throws Exception {
+		List<double[]> classes = new ArrayList<double[]>();
+		for (ScalabilityReport report: reports) {
+			for(ReportData data:report.values()) {
+				double[] sample = doubleArray(data.getMeasurements()); 
+				classes.add(sample);
+			}
+			String test = TestUtils.oneWayAnovaTest(classes, 0.01)?"not equivalent":"equivalent";
+			System.out.println("samples in " + report.getName() + " are " + test);
+		}
+
+	}
+	
+	private double[] doubleArray(List<Number> list) {
+		double[] result = new double[list.size()];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = list.get(i).doubleValue();
+		}
+		return result;
+	}
+
+}
