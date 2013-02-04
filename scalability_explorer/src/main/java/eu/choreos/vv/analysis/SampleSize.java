fdc8ef83bfa0d13306d933bf08a3f248440e4f2b
Paulo Moura <pbmoura@gmail.com>
2013-02-03 22:57:46 -0200

    calculates sample size from a pilot

---
+++ scalability_explorer/src/main/java/eu/choreos/vv/analysis/SampleSize.java
@@ -0,0 +1,41 @@
+package eu.choreos.vv.analysis;
+
+import java.util.List;
+
+import org.apache.commons.math.distribution.TDistribution;
+import org.apache.commons.math.distribution.TDistributionImpl;
+import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
+
+import eu.choreos.vv.aggregations.DescriptiveStatisticsFactory;
+import eu.choreos.vv.data.ScalabilityReport;
+
+public class SampleSize implements Analyser {
+	
+	private double d;
+	
+	/**
+	 * Calculates the appropriate sample size for the experiment.
+	 * @param precision a degree of precision given in the same unit as the measurements 
+	 */
+	public SampleSize(double precision) {
+		d = precision;
+	}
+
+	@Override
+	public void analyse(List<ScalabilityReport> reports) throws Exception {
+		for(ScalabilityReport report: reports) {
+			for(Number key: report.keySet()) {
+				List<Number> sample = report.get(key).getMeasurements(); 
+				int n = sample.size();
+				DescriptiveStatistics ds = DescriptiveStatisticsFactory.create(sample);
+				double s = ds.getStandardDeviation();
+				TDistribution dist = new TDistributionImpl(n-1);
+				double t = dist.cumulativeProbability(0.05);
+				long sampleSize = Math.round(Math.ceil(Math.pow((t*s)/d, 2)));
+				System.out.println("sample size must be " + sampleSize);
+			}
+		}
+
+	}
+
+}
