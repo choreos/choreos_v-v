fe70505714ae5aff00b7dc284c4dad1a3ea37c49
Alin Pandichi <alin.pandichi@codefactorygroup.com>
2014-07-15 16:48:29 +0300

    Added wsdl file and response xml in order to remove network dependency

--- rehearsal/src/test/java/eu/choreos/vv/clientgenerator/ParseXmlCdataResponseTest.java
+++ rehearsal/src/test/java/eu/choreos/vv/clientgenerator/ParseXmlCdataResponseTest.java
@@ -2,31 +2,68 @@ package eu.choreos.vv.clientgenerator;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 
-import javax.xml.xpath.XPathExpressionException;
-
+import org.apache.commons.io.IOUtils;
 import org.apache.xmlbeans.XmlException;
 import org.junit.Test;
 
 import eu.choreos.vv.exceptions.FrameworkException;
 import eu.choreos.vv.exceptions.InvalidOperationNameException;
+import eu.choreos.vv.exceptions.NoMockResponseException;
+import eu.choreos.vv.exceptions.NoReplyWithStatementException;
 import eu.choreos.vv.exceptions.WSDLException;
+import eu.choreos.vv.servicesimulator.MockResponse;
+import eu.choreos.vv.servicesimulator.WSMock;
 
 public class ParseXmlCdataResponseTest {
 
 	@Test
-	public void shouldReadSuccessFromResponse() throws WSDLException, XmlException, IOException, FrameworkException, InvalidOperationNameException, XPathExpressionException, NoSuchFieldException {
-		String wsdl = "http://www.webservicex.net/globalweather.asmx?WSDL";
-		WSClient wsClient = new WSClient(wsdl);
+	public void shouldReadSuccessFromResponse() throws WSDLException, XmlException, IOException, FrameworkException, InvalidOperationNameException, NoSuchFieldException, NoMockResponseException,
+			NoReplyWithStatementException {
+		String wsdl = "file://" + System.getProperty("user.dir") + "/resource/globalweather.wsdl";
 
+		WSMock wsMock = new WSMock("globalweather", "4321", wsdl);
+		MockResponse mockResponse = new MockResponse();
+		String path = System.getProperty("user.dir") + "/resource/getWeatherResponse.xml";
+		mockResponse.whenReceive(request()).replyWith(fileContent(path));
+		wsMock.returnFor("GetWeather", mockResponse);
+		wsMock.start();
+
+		WSClient wsClient = new WSClient("http://localhost:4321/globalweather?wsdl");
+		Item response = wsClient.request("GetWeather", request());
+		String content = response.getContent("GetWeatherResult");
+		assertTrue(content.contains("<Status>Success</Status>"));
+	}
+
+	private Item request() {
 		Item request = new ItemImpl("GetWeather");
 		request.addChild("CountryName").setContent("United States");
 		request.addChild("CityName").setContent("New York");
+		return request;
+	}
 
-		Item response = wsClient.request("GetWeather", request);
-		String content = response.getContent("GetWeatherResult");
+	public String fileContent(String filePath) throws FileNotFoundException {
+		InputStream stream = new FileInputStream(new File(filePath));
+		return readAsString(stream);
+	}
 
-		assertTrue(content.contains("<Status>Success</Status>"));
+	public String readAsString(InputStream inputStream) {
+		return readAsString(inputStream, "UTF-8");
+	}
+
+	public String readAsString(InputStream inputStream, String encoding) {
+		StringWriter writer = new StringWriter();
+		try {
+			IOUtils.copy(inputStream, writer, encoding);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return writer.toString();
 	}
 }
